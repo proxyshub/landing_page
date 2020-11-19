86e7de1bf97f40e262d6ed08917e0246d8f3f0aa
proxyshub <74704860+proxyshub@users.noreply.github.com>
2020-11-19 11:30:31 +0200

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -4,27 +4,27 @@
  This is the link to your domain...it redirects the user to your myaccount page.
  It is used on the site on buttons like: Buy now, Order now, Members, Buy HTTP proxies, etc.
 */
-export const MEMBERS_URL = "https://members.url.goes.here";
+export const MEMBERS_URL = "https://myaccount.proxyshub.com";
 
 /*
  Your site name - this appears in the footer
 */
-export const SITE_NAME = "Site name goes here";
+export const SITE_NAME = "ProxysHub";
 
 /*
  The name of you site that appears in Terms of service section - it can be you domain, your company name, etc.
 */
-export const TOS_SITE_NAME = 'whatever-you-need';
+export const TOS_SITE_NAME = 'ProxysHub';
 
 /*
  The date when the terms of service where last updated.
 */
-export const TOS_LAST_UPDATE = 'Apr-27-2018';
+export const TOS_LAST_UPDATE = 'Dec-02-2020';
 
 /*
  This is the link of your chat widget - it can be found on your tawk to account
 */
-export const TAWK_TO_WIDGET_URL = 'https://embed.tawk.to/5b61937de21878736ba285c5/default';
+export const TAWK_TO_WIDGET_URL = 'https://tawk.to/chat/5f7acccbf0e7167d00162ef5/default';
 
 /*
  This is the link to your backend services. It is used to get the data for prices and datacenters. Normally, you shouldn't touch this one
